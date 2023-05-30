47ca750f8e9dc9017e5432e7742c9b8770b4b519
malgosiaas <m.szeler@student.uw.edu.pl>
2023-05-30 16:49:09 +0200

    obsluga plikow

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args ) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Give file with data");
+        String inputFile = myObj.nextLine();
+        FIleReader read = new FIleReader
+    }
+}
